29d2f766cb64bca922152a09d0426ebaa2783bd2
yoshito iwaisako <iwaisako@andfactory.co.jp>
2019-05-05 17:34:04 +0900

    カスタムApplicationクラスの作成

---
+++ app/src/main/java/jp/co/yiwaisako/rosoroso_lunch/MainApplication.kt
@@ -0,0 +1,11 @@
+package jp.co.yiwaisako.rosoroso_lunch
+
+import android.support.multidex.MultiDexApplication
+
+class MainApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // NOP
+    }
+}

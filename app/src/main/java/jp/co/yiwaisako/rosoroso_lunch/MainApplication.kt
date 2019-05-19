9e5369adb764c896acad139939b604dae6ec372d
yoshito iwaisako <iwaisako@andfactory.co.jp>
2019-05-19 10:40:58 +0900

    Timberの設定を追加

--- app/src/main/java/jp/co/yiwaisako/rosoroso_lunch/MainApplication.kt
+++ app/src/main/java/jp/co/yiwaisako/rosoroso_lunch/MainApplication.kt
@@ -1,11 +1,14 @@
 package jp.co.yiwaisako.rosoroso_lunch
 
 import android.support.multidex.MultiDexApplication
+import timber.log.Timber
 
 class MainApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        // NOP
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
